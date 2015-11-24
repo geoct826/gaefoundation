e5aefc6d2acec9a400d4125a39b816a2bb5ef843
George Tuan <George Tuan>
2015-11-24 07:07:04 -0600

    Basic Foundation site up

--- gulp.config.js
+++ gulp.config.js
@@ -7,12 +7,56 @@ module.exports = function() {
     var temp = './.tmp/';
 
     var config = {
+
+        // File Path
         alljs: [
             client + '**/*.js'
         ],
         build: goApp + '/static/',
-        images: client + 'img/**/*.*'
+        fonts: [
+            './bower_components/font-awesome/fonts/**/*.*',
+            './bower_components/material-design-iconic-font/dist/fonts/**/*.*'
+        ],
+        homeApp: homeApp,
+        homeBuild: goApp + 'static/home/',
+        homeCss: temp + 'home.css',
+        homeIndex: homeApp + 'index.html',
+        homeJs: homeApp + 'app/**/*.js',
+        images: client + 'img/**/*.*',
+        appScss: client + 'scss/**/*.scss',
+        scss: [
+            './bower_components/foundation-sites/scss',
+            './bower_components/normalize-scss/sass',
+            './bower_components/motion-ui/src'
+        ],
+        temp: temp,
+
+        // Settings
+        browserversion: [
+            'last 2 version',
+            '> 5%'
+        ],
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        },
+        // Optimized Files
+        homeOptimized: {
+            app: 'homeApp.js',
+            lib: 'homeLib.js'
+        },
+                
     };
 
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
+    };
+    
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -30,18 +30,104 @@ gulp.task('gae-serve', function() {
     }));
 });
 
+// Build Tasks
+gulp.task('wiredep', function() {
+    log('Wire up the bower css/js and app js into index.html');
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = require('wiredep').stream;
+
+    return gulp
+        .src(config.homeIndex)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.homeJs)))
+        .pipe(gulp.dest(config.homeApp));
+});
+
+gulp.task('inject', ['wiredep', 'styles'], function() {
+    log('Wire up the app css into index.html');
+
+    return gulp
+        .src(config.homeIndex)
+        .pipe($.inject(gulp.src(config.homeCss)))
+        .pipe(gulp.dest(config.homeApp));
+});
+
+gulp.task('optimize', ['inject'], function() {
+    log('Optimizing javascript, css, and html files');
+    
+    var assets = $.useref.assets({searchPath: './'});
+    var cssFilter = $.filter('**/*.css', {restore:true});
+    var jsAppFilter = $.filter('**/' + config.homeOptimized.app, {restore:true});
+    var jsLibFilter = $.filter('**/' + config.homeOptimized.lib, {restore:true});
+//    var templateCache = config.temp + config.templateCache.file;
+    
+    return gulp
+        .src(config.homeIndex)
+        .pipe($.plumber())
+        //.pipe($.inject(gulp.src(templateCache, {read: false}), {
+        //    starttag: '<!-- inject:templates:js -->'
+        //}))
+        .pipe(assets)    
+        // Filter and Minify CSS
+        .pipe(cssFilter)
+        .pipe($.minifyCss())
+        .pipe(cssFilter.restore)
+		// Filter and Minify Lib JS
+        .pipe(jsLibFilter)
+        .pipe($.uglify())
+        .pipe(jsLibFilter.restore)
+        // Filter, ngAnnotate and Minify App JS
+        .pipe(jsAppFilter)
+        //.pipe($.ngAnnotate({add:true}))
+        .pipe($.uglify())
+        .pipe(jsAppFilter.restore)
+        // Restore all assets
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest(config.homeBuild));
+});
+
 // Helper Tasks
 gulp.task('images', ['clean-images'], function() {
     log('Copying compressed images to goApp folder');
-    
+
     return gulp
         .src(config.images)
         .pipe($.imagemin({optimizationLevel: 4}))
         .pipe(gulp.dest(config.build + 'img'));
 });
 
+gulp.task('fonts', ['clean-fonts'], function () {
+    log('Copying fonts to goApp folder');
+
+    return gulp
+        .src(config.fonts)
+        .pipe(gulp.dest(config.build + 'font'));
+});
+
+gulp.task('styles', ['clean-styles'], function() {
+    log('Compiling SASS -> CSS');
+
+    return gulp
+        .src(config.appScss)
+        .pipe($.plumber())
+        .pipe($.sass({includePaths:config.scss}))
+        .pipe($.autoprefixer({browsers: config.browserversion}))
+        .pipe(gulp.dest(config.temp));
+});
+
 gulp.task('clean-images', function(done) {
     clean(config.build + 'img/**/*.*', done);
+    done();
+});
+
+gulp.task('clean-fonts', function(done) {
+    clean(config.build + 'font/**/*.*', done);
+    done();
+});
+
+gulp.task('clean-styles', function(done) {
+    clean(config.temp + '**/*.css', done);
     done();
 });
 
